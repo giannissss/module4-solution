1628b97e7dc0cb067a57b05a4e81b3e0316dd2ae
giannissss <75947612+giannissss@users.noreply.github.com>
2021-02-09 20:31:28 +0200

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -3,7 +3,7 @@
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
-(function (window) = {
+(function (window) {
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
@@ -12,7 +12,7 @@
 var byeSpeaker = function (name) {
   byeSpeaker.speak=speak(name);
 }
-var speakWord = "Good Bye";
+
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 var speakWord = "Good Bye";
 
